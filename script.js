8c1f5afe7908c87e12b3eba2b3291ac47e52c916
OmSuryawanshi9011 <131766249+OmSuryawanshi9011@users.noreply.github.com>
2025-01-30 23:10:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function getFormvalue() {
-    //Write your code here
-
+function getFormvalue(event) {
+	event.preventDefault();
+    var fname = document.querySelector('input[name="fname"]').value;
+	var lname = document.querySelector('input[name="lname"]').value;
+	alert(fname + " " + lname);
 }
